a080cba18d2d1b8dd5c42e60e9a22abfcfcc51ab
Nat Reed <natreed@Nats-MacBook-Pro.local>
2026-06-18 16:18:52 -0700

    Update calculator

--- Desktop/Learning Python/Calculator.py
+++ Desktop/Learning Python/Calculator.py
@@ -1,60 +1,53 @@
 import math 
 
 def main():
+    calculator()
+
+def calculator():
     
     ready = "False"
     nums = []
     operations = []
+    operation_input = ""
 
     output = 0 
-    count = 0
 
-    while ready == "F" or ready == "False":
-        
-        ##if (len(nums)) != 0:
+    while operation_input != "=":
+
         num_input = int(input("Enter an integer: "))
-        ready = input("Are You Finished? Enter T or F ")    
+        operation_input = str(input("Enter an Operation: +, -, /, *, = "))
         
-        if ready == "T" or ready == "True":
+        if operation_input == "=":
             nums.append(num_input)
             break
-        
-        operation_input = str(input("Enter an Operation: +, -, /, * "))
-        
         operations.append(operation_input)
         nums.append(num_input)
-        count += 1
-    
-    
-    while ready == "T" or ready == "True":
-        if not operations:
-            output = nums[0]
-            break
-        for i in range(len(nums)): 
-            if i == 0:
-                output = nums[i]
-            else:
-                a = i - 1
-                if operations[a] == "+":
-                    output += nums[i] 
-                elif operations[a] == "-":
-                    output -= nums[i]
-                elif operations[a] == "*":
-                    if output == 0:
-                        output = 1 * nums[i]
-                    else:
-                        output = output * nums[i]
-                elif operations[a] == "/":
-                    if output == 0:
-                        output =  nums[i] / 1
-                    else:
-                        output = output / nums[i]
-        break
+        
+    for i in range(len(nums)): 
+        if i == 0:
+            output = nums[i]
+        else:
+            a = i - 1
+            if operations[a] == "+":
+                output += nums[i] 
+            elif operations[a] == "-":
+                output -= nums[i]
+            elif operations[a] == "*":
+                if output == 0:
+                    output = 1 * nums[i]
+                else:
+                    output = output * nums[i]
+            elif operations[a] == "/":
+                if output == 0:
+                    output =  nums[i] / 1
+                else:
+                    output = output / nums[i]
+        
     
     print("The Answer is", output)
     ready("Do you wish to continue? ", ready)
                   
-main()
-
+if __name__ == "__main__":
+    main()
 
 
