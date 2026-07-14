49414bde142454292d8a14e3ab449606767ab29b
Nat Reed <natreed@Nats-MacBook-Pro.local>
2026-07-14 15:51:55 -0700

    Add due date, priority editing, CSS styling

--- Desktop/To-Do List Website/main.py
+++ Desktop/To-Do List Website/main.py
@@ -3,6 +3,7 @@ from fastapi import Form
 from fastapi import Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from datetime import date
 
 
 app = FastAPI()
@@ -25,11 +26,12 @@ async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request, "tasks": tasks})
 
 @app.post("/create_task")
-async def create_task_endpoint(request: Request, task_description: str = Form(...), task_priority: str = Form(...)):
+async def create_task_endpoint(request: Request, task_description: str = Form(...), task_priority: str = Form(...), task_due_date: date = Form(...)):
     task = Task()
     task.id = len(tasks)
     task.task_description = task_description
     task.task_priority = task_priority
+    task.task_due_date = task_due_date
     tasks.append(task)
     return templates.TemplateResponse("todo_list.html", {"request": request, "tasks": tasks})
 
@@ -47,7 +49,15 @@ async def update_task_priority_endpoint(request: Request, task_id: int = Form(..
         if task.id == task_id:
             task.task_priority = task_priority
             break
-        return templates.TemplateResponse("todo_list.html", {request: request, "tasks": tasks})
+        return templates.TemplateResponse("todo_list.html", {"request": request, "tasks": tasks})
+    
+@app.post("/update_task_due_date")
+async def update_task_due_date_endpoint(request: Request, task_id: int = Form(...), task_due_date: date = Form(...)):
+    for task in tasks:
+        if task.id == task_id:
+            task.task_due_date = task_due_date
+            break
+    return templates.TemplateResponse("todo_list.html", {"request": request, "tasks": tasks})
     
 @app.post("/delete_task")
 async def delete_task_endpoint(request: Request):
